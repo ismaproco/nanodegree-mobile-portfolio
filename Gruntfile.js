5bfc9946b82d728c2f3c2e5ea6dc0bf4fc4dfcea
ismael <ismael.jimenez@gmail.com>
2015-03-07 12:46:29 -0600

    update grunt task files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,45 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            css: {
+                src: 'src/css/style.css',
+                dest: 'build/css/style.css'
+            },
+            css: {
+                src: 'src/css/print.css',
+                dest: 'build/css/print.css'
+            },
+            css: {
+                src: 'src/css/bootstrap-grid.css',
+                dest: 'build/css/bootstrap-grid.css'
+            },
+            css: {
+                src: 'src/css/pizza-style.css',
+                dest: 'build/css/pizza-style.css'
+            },
+        },
+        htmlmin: {                                     // Task
+            dist: {                                      // Target
+              options: {                                 // Target options
+                removeComments: true,
+                collapseWhitespace: true
+              },
+              files: {                                   // Dictionary of files
+                'build/pizza.html': 'src/pizza.html',     // 'destination': 'source'
+                'build/index.html': 'src/index.html',
+                'build/project-2048.html': 'src/project-2048.html',
+                'build/project-mobile.html': 'src/project-mobile.html',
+                'build/project-webperf.html': 'src/project-webperf.html'
+              }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+    grunt.registerTask('default', ['concat:css', 'cssmin:css', 'concat:js', 'uglify:js','htmlmin']);
 };
