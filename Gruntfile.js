26e1b53738cf80d7f3b5a578984413b10ea489bc
Savathos <Savathos@Savathoss-MacBook-Air.local>
2015-03-07 18:50:18 -0600

    Added grunt task to manage the build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,18 @@
+var mozjpeg = require('imagemin-mozjpeg');
+
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         cssmin: {
-            css: {
-                src: 'src/css/style.css',
-                dest: 'build/css/style.css'
-            },
-            css: {
-                src: 'src/css/print.css',
-                dest: 'build/css/print.css'
-            },
-            css: {
-                src: 'src/css/bootstrap-grid.css',
-                dest: 'build/css/bootstrap-grid.css'
-            },
-            css: {
-                src: 'src/css/pizza-style.css',
-                dest: 'build/css/pizza-style.css'
-            },
+            target: {
+                    files: [{
+                    expand: true,
+                    cwd: 'src/css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'build/css',
+                    ext: '.css'
+                }]
+            }
         },
         htmlmin: {                                     // Task
             dist: {                                      // Target
@@ -33,13 +28,42 @@ module.exports = function (grunt) {
                 'build/project-webperf.html': 'src/project-webperf.html'
               }
             }
+        },
+        uglify: {
+            options: {
+              mangle: false,
+              compress: false
+            },
+            my_target: {
+                files: {
+                    'build/js/main.js': ['src/js/main.js'],
+                    'build/js/perfmatters.js': ['src/js/perfmatters.js']
+                }
+            }
+        },
+        imagemin: {                          // Task
+            dynamic: {                         // Another target
+                options: {                       // Target options
+                    optimizationLevel: 3,
+                    svgoPlugins: [{ removeViewBox: false }],
+                    use: [mozjpeg()]
+                },
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'src/img',                   // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'build/img'                  // Destination path prefix
+                }]
+            }
         }
     });
+
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask('default', ['concat:css', 'cssmin:css', 'concat:js', 'uglify:js','htmlmin']);
+    grunt.registerTask('default', ['cssmin', 'uglify','htmlmin','imagemin']);
 };
